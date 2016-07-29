212aa029ae6ab5bae5954daae3fe9c66f463cc83
Sriram Panyam <sri.panyam@gmail.com>
2016-07-28 21:14:48 -0700

    Upped version

--- onering/__init__.py
+++ onering/__init__.py
@@ -1,10 +1,8 @@
 
-VERSION = (0, 0, 5)
+VERSION = (0, 0, 6)
 
 def get_version(version = None):
     version = version or VERSION
     return ".".join(map(str, list(version)))
 
 __version__ = get_version(VERSION)
-
-from core import *
