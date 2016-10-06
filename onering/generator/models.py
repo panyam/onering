f16faa767664a98f970d341575998eda1e5c1ab5
Sriram Panyam <sri.panyam@gmail.com>
2016-10-06 10:33:40 -0700

    Statement also becomes a view model so we can add view related things here - may be even instructions later on

--- onering/generator/models.py
+++ onering/generator/models.py
@@ -42,24 +42,31 @@ class TransformerGroupViewModel(object):
         self.namespace = ns
         self.import_types = []
         self.annotations = tlannotations.Annotations(tgroup.annotations)
-        self.transformers = [TransformerViewModel(t,self, backend) for t in tgroup.all_transformers]
+        self.transformers = [TransformerViewModel(t, self, backend) for t in tgroup.all_transformers]
 
 
 class TransformerViewModel(object):
-    def __init__(self, transformer, tgroup, backend):
+    def __init__(self, transformer, tgroupvm, backend):
         self.backend = backend
-        self.context = tgroup.context
-        self.tgroup = tgroup
+        self.context = tgroupvm.context
+        self.tgroupvm = tgroupvm
         self.name = transformer.fqn
         self.instructions, self.symtable = orgencore.generate_ir_for_transformer(transformer, self.context)
         self.src_name,self.src_namespace,self.src_fqn = utils.normalize_name_and_ns(transformer.src_fqn, "")
         self.src_varname = transformer.src_varname
         self.dest_varname = transformer.dest_varname
         self.dest_name,self.dest_namespace,self.dest_fqn = utils.normalize_name_and_ns(transformer.dest_fqn, "")
-        self.all_statements = transformer.all_statements
+        self.all_statements = [StatementViewModel(stmt, self, backend) for stmt in  transformer.all_statements]
 
     def render(self):
         templ = self.backend.load_template(self.backend.backend_annotation.first_value_of("template") or "transformers/java/default_transformer")
-        out = templ.render(transformer = self, tgroup = self.tgroup, backend = self.backend)
+        out = templ.render(transformer = self, tgroup = self.tgroupvm, backend = self.backend)
         return out
+
+
+class StatementViewModel(object):
+    def __init__(self, statement, transformervm, backend):
+        self.statement = statement
+        self.transformervm = transformervm
+        self.backend = backend
 
