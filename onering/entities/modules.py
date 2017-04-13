8458a11080ee9f33c077af15d475779e5d3e623a
Sriram Panyam <spanyam@linkedin.com>
2017-04-13 15:07:42 -0700

    Moving imports to module - may go to Entity

--- onering/entities/modules.py
+++ onering/entities/modules.py
@@ -6,3 +6,4 @@ class Module(Entity):
     TAG = "MODULE"
     def __init__(self, name, parent = None, annotations = None, docs = ""):
         Entity.__init__(self, name, parent, annotations, docs)
+        self.imports = []
