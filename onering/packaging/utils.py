1c72a17413bbe2df1df261442889d4980f26e1a8
Sriram Panyam <sri.panyam@gmail.com>
2017-06-23 10:12:48 -0700

    api checking removed for now

--- onering/packaging/utils.py
+++ onering/packaging/utils.py
@@ -29,10 +29,3 @@ def is_fun_entity(entity):
 def is_function_mapping_entity(entity):
     return isinstance(entity, tlcore.Fun) and entity.is_external and not entity.is_type_fun
 
-def is_api_functype(entity):
-    if type(entity) is not tlcore.Fun: return False
-    if not entity.parent or not isinstance(entity.parent, ormods.Module): return False
-    type_annotations = entity.parent.annotations.get_first("type")
-    if type_annotations is None: return False
-    if type_annotations .value != "api": return False
-    return True
